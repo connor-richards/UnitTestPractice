8c2d08c4c8404dd223cf7c8244a7f419dee54f57
connor-richards <connor.a.t.richards@gmail.com>
2020-04-25 18:01:39 -0700

    more sorting tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -146,3 +146,45 @@ TEST(PracticeTest, first_third_sort)
     }
     ASSERT_TRUE(passed);
 }
+
+TEST(PracticeTest, all_same_sort)
+{
+    Practice obj;
+    int first = 1;
+    int second = 1;
+    int third = 1;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 1 && second == 1 && third == 1) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+
+TEST(PracticeTest, negative_sort)
+{
+    Practice obj;
+    int first = -1;
+    int second = -2;
+    int third = -3;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == -1 && second == -2 && third == -3) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
+
+TEST(PracticeTest, some_negative_sort)
+{
+    Practice obj;
+    int first = 1;
+    int second = -2;
+    int third = 3;
+    bool passed = false;
+    obj.sortDescending(first , second, third);
+    if(first == 3 && second == 1 && third == -2) {
+      passed = true;
+    }
+    ASSERT_TRUE(passed);
+}
