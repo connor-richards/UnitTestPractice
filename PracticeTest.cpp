d5410c51bec4fc6ffa828f783694eccadee8e7e0
connor-richards <connor.a.t.richards@gmail.com>
2020-04-25 17:20:20 -0700

    add some tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -20,3 +20,45 @@ TEST(PracticeTest, is_simple_palindrome)
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
+
+TEST(PracticeTest, is_not_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("ba");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_large_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racecar");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_not_large_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("thisisnotasiht");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_spaced_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("race car");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_onechar_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("a");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_empty_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
